e85a3e95f64369964095b84192ad6ecbd0174082
Snail Mail <>
2023-04-24 23:37:17 +0000

    init.

---
+++ dynip-server/src/test/java/cc/tools/dynip/server/IpServerTest.java
@@ -0,0 +1,12 @@
+package cc.tools.dynip.server;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.lang.reflect.*;
+
+public class IpServerTest {
+    @Test
+    public void placeholderTest() {
+	assertTrue("IpServer Tests Placeholder", 1 == 1);
+   }
+}
